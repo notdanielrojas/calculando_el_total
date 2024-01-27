5891f73111f50a2937a24a19d6344f7cfbe7d5cb
Daniel Rojas <notdanielrojas@gmail.com>
2024-01-27 00:50:14 -0300

    script fixed

--- index.js
+++ index.js
@@ -2,3 +2,30 @@ precio = 400000;
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio;
+
+function botonAumentar () {
+    cantidadSpan = document.querySelector('.cantidad'); /* Selecciono elemento DOM */
+    cantidad = Number(cantidadSpan.innerHTML); /* Transformo DOM de String a Número */
+    cantidad = cantidad + 1; /* Valor asignado */
+    cantidadSpan.innerHTML = cantidad; /* Modificado HTML */
+
+    totalSpan = document.querySelector('.valor-total'); /* Selecciono elemento DOM */
+    precio = Number(document.querySelector('.precio-inicial').innerHTML); /* Transformo DOM de String a Número */
+    total = cantidad * precio; /* Valor asignado */
+    totalSpan.innerHTML = total; /* Modificado HTML */ 
+}
+
+function botonDisminuir () {
+    cantidadSpan = document.querySelector('.cantidad'); /* Selecciono elemento DOM */
+    cantidad = Number(cantidadSpan.innerHTML); /* Transformo texto a número */
+    cantidad = cantidad - 1; /* Valor asignado */
+    cantidad = Math.max(0, cantidad - 1); /* Impide que el valor sea negativo */
+    cantidadSpan.innerHTML = cantidad; /* Modificado HTML */
+
+
+    totalSpan = document.querySelector('.valor-total'); /* Selecciono elemento DOM */
+    precio = Number(document.querySelector('.precio-inicial').innerHTML); /* Transformo DOM de String a Número */
+    total = cantidad * precio; /* Valor asignado */
+    totalSpan.innerHTML = total; /* Modificado HTML */  
+    
+}
