a57435899e773922967679a6965288dfbb7444dd
Daniel Rojas <notdanielrojas@gmail.com>
2024-01-29 18:27:15 -0300

    modificado js

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-precio = 400000;
+precio = 400000; /* Valor Preico */
 
-precioSpan = document.querySelector(".precio-inicial");
+precioSpan = document.querySelector(".precio-inicial"); /* Selecciono elemento DOM ".precio-inicial" */
 precioSpan.innerHTML = precio;
 
 function botonAumentar () {
-    cantidadSpan = document.querySelector('.cantidad'); /* Selecciono elemento DOM */
+    cantidadSpan = document.querySelector(".cantidad"); /* Selecciono elemento DOM '.cantidad' */
     cantidad = Number(cantidadSpan.innerHTML); /* Transformo DOM de String a Número */
     cantidad = cantidad + 1; /* Valor asignado */
     cantidadSpan.innerHTML = cantidad; /* Modificado HTML */
 
-    totalSpan = document.querySelector('.valor-total'); /* Selecciono elemento DOM */
-    precio = Number(document.querySelector('.precio-inicial').innerHTML); /* Transformo DOM de String a Número */
+    totalSpan = document.querySelector(".valor-total"); /* Selecciono elemento DOM ".valor-total"*/
+    precio = Number(document.querySelector(".precio-inicial").innerHTML); /* Selecciono y transformo DOM ".precio-inicial" de String a Número */
     total = cantidad * precio; /* Valor asignado */
     totalSpan.innerHTML = total; /* Modificado HTML */ 
 }
 
 function botonDisminuir () {
-    cantidadSpan = document.querySelector('.cantidad'); /* Selecciono elemento DOM */
-    cantidad = Number(cantidadSpan.innerHTML); /* Transformo texto a número */
+    cantidadSpan = document.querySelector(".cantidad"); /* Selecciono elemento DOM ".cantidad" */
+    cantidad = Number(cantidadSpan.innerHTML); /* Transformo DOM de String a Número */
     cantidad = cantidad - 1; /* Valor asignado */
     cantidad = Math.max(0, cantidad); /* Impide que el valor sea negativo */
     cantidadSpan.innerHTML = cantidad; /* Modificado HTML */
 
 
-    totalSpan = document.querySelector('.valor-total'); /* Selecciono elemento DOM */
-    precio = Number(document.querySelector('.precio-inicial').innerHTML); /* Transformo DOM de String a Número */
+    totalSpan = document.querySelector(".valor-total"); /* Selecciono elemento DOM ".valor-total"*/
+    precio = Number(document.querySelector(".precio-inicial").innerHTML); /* Selecciono y transformo DOM '.precio-inicial' de String a Número */
     total = cantidad * precio; /* Valor asignado */
     totalSpan.innerHTML = total; /* Modificado HTML */  
     
